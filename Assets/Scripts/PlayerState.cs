d4b64489c53d238e4f7ade565ff3826b4018d489
PadBol1742 <padbol1030@gmail.com>
2026-06-29 12:07:33 +0200

    setup du portoloin. Fin de boucle victorieuse fonctionnelle. + déplacement de certaines responsabilité sur le PlayerState script

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -1,28 +1,53 @@
-using System;
 using UnityEngine;
 
 public class PlayerState : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] HeroData data;
+    [SerializeField] GridManager gridManager;
+    [SerializeField] CombatManager combatManager;
+
+    [Header("Suscriptions")]
     [SerializeField] GameManager gameManager;
-
-    public Hero hero;
+    [SerializeField] PlayerMovement playerMovement;
     
+    //public Hero PlayerUnit => _playerUnit;
 
+    private Hero _playerUnit;
+    private Tile _currentTile;
+        
     private void OnEnable()
     {
-        gameManager.OnStartNewLevel += HandleStartNewLevel;        
+        gameManager.OnStartNewLevel += HandleStartNewLevel;
+        playerMovement.OnPlayerMoved += HandlePlayerMoved;
     }
 
     private void OnDisable()
     {
         gameManager.OnStartNewLevel -= HandleStartNewLevel;
+        playerMovement.OnPlayerMoved -= HandlePlayerMoved;
+    }
+
+    private void HandlePlayerMoved(Vector2Int playerPosition)
+    {
+        _currentTile = gridManager.Grid[playerPosition];
+        switch (_currentTile.TileState)
+        {
+            case TileState.Exit:
+                playerMovement.SetCanMove(false);
+                gameManager.EndLevel(_playerUnit.IsAlive);
+                break;
+
+            case TileState.Encounter:
+                playerMovement.SetCanMove(false);
+                StartCoroutine(combatManager.StartCombat(_playerUnit, _currentTile.Enemy));
+                break;
+        }
     }
 
     private void HandleStartNewLevel()
     {
-        hero = new Hero(
-
+        _playerUnit = new Hero(
             data.archetype,
             data.heroName,
             data.hpMax,
